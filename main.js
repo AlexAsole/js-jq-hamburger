10b92963ea913af561f1a375db6202d4dda884db
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-01-12 15:39:53 +0100

    show-fadeIn     hide-fadeOut

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.fa-bars').click(function() {
+  // $('.hamburger-menu').show()
+  $('.hamburger-menu').fadeIn()
+})
+$('.close').click(function() {
+  // $('.hamburger-menu').hide()
+  $('.hamburger-menu').fadeOut()
+})
